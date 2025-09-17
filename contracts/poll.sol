da2c16298ac60e5f2da024053791c5307555ef25
Dav16Akin <210805089@live.unilag.edu.ng>
2025-09-17 11:02:45 +0100

    first commit

---
+++ contracts/poll.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract Test {
+    uint256 myNumber;
+    bool isNumber;
+    bytes32 password;
+    string name;
+
+    string[] names;
+    mapping(uint256 => bool) ids;
+
+    address ad;
+}
